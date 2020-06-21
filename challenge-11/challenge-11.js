054ece0584ee963540fb0008e274bcd5c880a767
Emanuel Souza <emanuel-souza1@hotmail.com>
2020-06-21 15:53:53 -0300

    ihul

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,3 +1,4 @@
+(function(){
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
@@ -7,6 +8,11 @@ loop, mostre no console a mensagem:
 'Entrou ao menos uma vez!'
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
+let once = false;
+do{
+    console.log("Entrou ao menos uma vez!");
+
+}while(once);
 // ?
 
 /*
@@ -14,6 +20,12 @@ Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
+let person = {
+    name : 'Biricotico',
+    age : 19,
+    weight : 50,
+    birthday : "09/09/2001",
+};
 // ?
 
 /*
@@ -25,6 +37,12 @@ esse objeto tem.
 Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
+let count = 0;
+for(data in person){
+    console.log("The "+data+" of person is "+person[data]);
+    count++;
+}
+console.log("The person has "+count+" properties");
 // ?
 
 /*
@@ -34,6 +52,14 @@ Se verdadeiro, retornar `true`. Senão, retornar false.
 Após a função, mostrar a mensagem no console:
 'The person has more than 25 years old? [TRUE/FALSE]'
 */
+function moreThan(age){
+
+    if(person["age"] > age){
+        return true;
+    }
+    return false;
+}
+console.log("The person has more than 25 years old?"+ moreThan(25));
 // ?
 
 /*
@@ -41,7 +67,15 @@ Faça um loop de 0 a 20, que adicione cada número como um item de um
 array chamado `numbers`. Se o contador for maior que 10, saia do loop.
 Mostre no console os números no array.
 */
-console.log( 'De 0 a 10:' );
+let numbers = [];
+for(i=0; i<=20; i++){
+    if(i>10){
+        break;
+    }
+    numbers.push(i);
+
+}
+console.log( 'De 0 a 10:', numbers );
 // ?
 
 /*
@@ -50,5 +84,14 @@ criado acima, só precisa ser reiniciado) números de 0 a 20, inclusive
 esses. Se o número for ímpar, pular para o próximo número.
 Mostrar no console os números do array.
 */
-console.log( 'Pares de 0 a 20:' );
+numbers = [];
+for(i=0; i<=20; i++){
+    if(i%2 !== 0){
+        continue;
+    }
+    numbers.push(i);
+
+}
+console.log( 'Pares de 0 a 20:', numbers );
 // ?
+})()
--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -1,3 +1,4 @@
+(function() {
 /*
 Envolva todo o conteúdo desse arquivo em uma IIFE.
 */
@@ -10,18 +11,24 @@ Crie um objeto chamado `person`, com as propriedades:
 Preencha cada propriedade com os seus dados pessoais, respeitando o tipo
 de valor para cada propriedade.
 */
+let person = {
+    name : "Biricotico",
+    lastname : "Dovrau",
+    age : 26,
+};
 // ?
-console.log( 'Propriedades de "person":' );
+console.log( 'Propriedades de "person":', person );
 
 /*
 Mostre no console, em um array, todas as propriedades do objeto acima.
 Não use nenhuma estrutura de repetição, nem crie o array manualmente.
 */
 // ?
-
+console.log(Object.keys(person));
 /*
 Crie um array vazio chamado `books`.
 */
+let books = [];
 // ?
 
 /*
@@ -30,71 +37,108 @@ seguintes propriedades:
 `name`: String
 `pages`: Number
 */
+books.push({ name: "algo", pages: 16,});
+books.push({ name: "uhul",pages: 20,});
+books.push({ name: "legal",pages: 34,});          
 // ?
-console.log( '\nLista de livros:' );
+
 
 /*
 Mostre no console todos os livros.
 */
+console.log( '\nLista de livros:', books );
 // ?
 
 console.log( '\nLivro que está sendo removido:' );
 /*
 Remova o último livro, e mostre-o no console.
 */
+let removi = books.pop();
+console.log(removi);
 // ?
 
 console.log( '\nAgora sobraram somente os livros:' );
 /*
 Mostre no console os livros restantes.
 */
+console.log(books);
 // ?
 
 /*
 Converta os objetos que ficaram em `books` para strings.
 */
+let converti = JSON.stringify(books);
 // ?
 console.log( '\nLivros em formato string:' );
 
 /*
 Mostre os livros nesse formato no console:
 */
+console.log(converti);
 // ?
 
 /*
 Converta os livros novamente para objeto.
 */
+let desconverti = JSON.parse(converti);
 // ?
-console.log( '\nAgora os livros são objetos novamente:' );
+console.log( '\nAgora os livros são objetos novamente:', desconverti );
 
 /*
 Mostre no console todas as propriedades e valores de todos os livros,
 no formato abaixo:
     "[PROPRIEDADE]: [VALOR]"
 */
+
+for(i=0; i < books.length; i++){
+
+    for( prop in books[i]){
+
+        console.log(prop+":"+books[i][prop]);
+    }
+}
+
 // ?
 
 /*
 Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
 seu nome. Adicione seu nome completo no array.
 */
+let myName = [];
+myName.push("E");
+myName.push("M");
+myName.push("A");
+myName.push("N");
+myName.push("U");
+myName.push("E");
+myName.push("L");
 // ?
-console.log( '\nMeu nome é:' );
+console.log( '\nMeu nome é: '+ myName);
+
 
 /*
 Juntando todos os itens do array, mostre no console seu nome.
 */
+console.log( '\nMeu nome juntando é: '+ myName.join(""));
 // ?
-
-console.log( '\nMeu nome invertido é:' );
 
 /*
 Ainda usando o objeto acima, mostre no console seu nome invertido.
 */
+let reverse_name = [];
+for (i=myName.length-1; i>=0; i--){
+
+    reverse_name.push(myName[i]);
+}
+console.log( '\nMeu nome invertido é: '+reverse_name.join(""));
+
 // ?
 
 console.log( '\nAgora em ordem alfabética:' );
 /*
 Mostre todos os itens do array acima, odenados alfabéticamente.
 */
+console.log("normal: "+myName.sort())
+console.log("reverso: "+reverse_name.sort())
 // ?
+})()
